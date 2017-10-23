a2db65ed79c579eee7ecf1c82b4e1c42684e59cd
James Misner <jwm80@pitt.edu>
2017-10-23 16:15:26 -0400

    Modify gradle build file

--- src/main/java/Exercise3.java
+++ src/main/java/Exercise3.java
@@ -8,8 +8,14 @@ public class Exercise3 {
 		return triNum;
 	}
 
+	public int lazyCatererSeq(int n){
+		return (int)(Math.pow(n, 2) + n + 2) / 2;
+	}
+
 	public static void main(String[] args) {
 		Exercise3 tri = new Exercise3();
+		//Exercise3 lzc = new Exercise3();
+		//System.out.println(lzc.lazyCatererSeq(5));
 		System.out.println(tri.triangularNumberSeq(5));
 	}
 }
