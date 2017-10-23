ca9609a08ed3f720cb7af46ef5ad473738760d2a
Matthew Hoesch <matthewhoesch@pitt.edu>
2017-10-23 15:51:22 -0400

    Add files via upload

---
+++ src/main/group/java/Exercise3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Exercise3 {
+	public int triangularNumberSeq(int n){
+		int triNum = 0;
+		for (int i = 0; i< n; i++) {
+			triNum = n*(n+1)/2;
+		}
+		return triNum;
+	}
+	
+	public int lazySeq(int n){
+		return Math.pow(n, 2);
+	}
+	public static void main(String[] args) {
+		Exercise3 tri = new Exercise3();
+		System.out.println(tri.triangularNumberSeq(5));
+	}
+}
